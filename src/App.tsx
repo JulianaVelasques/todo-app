7a3cfe3a91adbf32547c0ac2aef8d16521b56d50
JulianaVelasques <juliabalta16@gmail.com>
2023-02-23 22:02:09 -0300

    fix: config project's colors and font; change icons to another folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold font-josefin-sans underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bright-blue': 'hsl(220, 98%, 61%)',
+        'check-background':
+          'linear-gradient(hsl(192, 100%, 67%), hsl(280, 87%, 65%))',
+        'light-theme-gray': 'hsl(0, 0%, 98%)',
+        'very-light-theme-grayish-blue': 'hsl(236, 33%, 92%)',
+        'light-theme-grayish-blue': 'hsl(233, 11%, 84%)',
+        'light-theme-dark-grayish-glue': ' hsl(235, 19%, 35%)',
+        'dark-theme-blue': 'hsl(235, 21%, 11%)',
+        'dark-theme-desaturated-blue': ' hsl(235, 24%, 19%)',
+        'dark-theme-light-grayish-blue': 'hsl(234, 39%, 85%)',
+        'dark-theme-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+        'dark-theme-grayish-blue': 'hsl(234, 11%, 52%)',
+        'dark-theme-very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+      },
+      fontFamily: {
+        'josefin-sans': ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
