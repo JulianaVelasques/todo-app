80da61e373cd6c710803ed318e6481a90b2b5446
JulianaVelasques <juliabalta16@gmail.com>
2023-02-25 23:13:45 -0300

    style: the input todo component, add background img mobile and start todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { InputTodo } from './components/InputTodo';
+import bgMobileLight from './assets/bg-mobile-light.jpg';
+import moonIcon from './assets/icon-moon.svg';
 
 function App() {
   return (
-    <div className="flex items-center justify-center font-josefin-sans ">
-      <div>
-        <InputTodo />
+    <div className="flex flex-col items-center justify-center font-josefin-sans">
+      <img src={bgMobileLight} alt="" className="absolute top-0 z-[-1]" />
+      <div className="flex items-center w-[100%] mt-10 justify-between px-6">
+        <h1 className="text-white tracking-[0.5rem] text-[1.5rem]">TODO</h1>
+
+        <img src={moonIcon} alt="turn dark on" width={20} />
       </div>
+      <InputTodo />
     </div>
   );
 }
--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -1,12 +1,7 @@
 export function InputTodo() {
   return (
-    <div className="flex items-center gap-1 bg-white pl-5 w-[307px] h-12">
-      <div className="relative">
-        <input
-          type="checkbox"
-          className="w-5 h-5 rounded-full border-[#E3E4F1] checked:bg-gradient-to-br checked:from-[#57ddff] checked:to-[#c058f3] focus:ring-transparent before:content-[''] before:hidden before:absolute before:top-[62%] before:left-[50%] before:translate-y-[-50%] before:translate-x-[-50%] before:pointer-events-none before:bg-check-icon before:bg-contain before:bg-no-repeat before:w-[55%] before:h-[55%] checked:before:block"
-        />
-      </div>
+    <div className="flex items-center gap-1 bg-white pl-5 w-[327px] h-12 mt-[35px] rounded-md">
+      <span className="h-[20px] w-[20px] border border-solid rounded-full border-[#E3E4F1]"></span>
 
       <input
         type="text"
